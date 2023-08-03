35623e12fe7298130a01462af16f44a85fa3d88c
Travis James <itravisjames@outlook.com>
2023-08-02 21:52:23 -0700

    - feat: Add BookmarksProvider to manage bookmark state
    Wrap the entire application with BookmarksProvider to handle bookmark state and enable feature to bookmark book selections.

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { BookmarksProvider } from './contexts/bookmarks-context';
 import Home from './Home';
 
 ReactDOM.render(
   <BrowserRouter>
-    <Home />
+    <BookmarksProvider>
+      <Home />
+    </BookmarksProvider>
   </BrowserRouter>,
   document.getElementById('root')
 );
