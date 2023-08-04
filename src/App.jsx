ec5f674a8d3e134dfbb640e80cbd8426f72e7c4d
Travis James <itravisjames@outlook.com>
2023-08-04 11:43:43 -0700

    Set up basic routing with React Router in App component and connected App component to index.js for rendering.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, /* Add Route once there are paths*/ } from 'react-router-dom';
+
+function App() {
+  return (
+      <Router>
+        <Routes>
+        </Routes>
+      </Router>
+  );
+}
+
+export default App;
