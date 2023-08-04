85d6450b5bb5e4135ac834d65ef5eb54538aa42c
Travis James <itravisjames@outlook.com>
2023-08-04 11:51:26 -0700

    Implemented routing logic in App.jsx to enable navigation between Home, BookResults, and BookInfo components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, /* Add Route once there are paths*/ } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import BookResults from './components/BookResults';
+import BookInfo from './components/BookInfo';
 
 function App() {
   return (
       <Router>
         <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books" element={<BookResults />} />
+        <Route path="/books/:id" element={<BookInfo />} />
         </Routes>
       </Router>
   );
